6c966c77c8bc6fb013337cb6bbb1b32d100913c2
Diego Perdomo Salcedo <diegoperdomosalcedo@Diegos-MacBook-Air.local>
2022-03-24 18:30:30 -0600

    addedcolor and changed the size of the symbols, some code for the next step

--- tictactoeA01709150/tictactoe.py
+++ tictactoeA01709150/tictactoe.py
@@ -13,37 +13,45 @@ def grid():
 
 def drawx(x, y):
     """Draw X player."""
-    line(x, y, x + 133, y + 133)
-    line(x, y + 133, x + 133, y)
+    color("blue")
+    line(x, y, x + 100, y + 100)
+    line(x, y + 100, x + 100, y)
 
 
 def drawo(x, y):
     """Draw O player."""
+    color("red")
     up()
-    goto(x + 67, y + 5)
+    goto(x + 58, y + 2)
     down()
-    circle(62)
+    circle(55)
 
 
 def floor(value):
     """Round value down to grid with square size 133."""
-    return ((value + 200) // 133) * 133 - 200
+    return ((value + 185) // 133) * 133 - 185
 
 
 state = {'player': 0}
 players = [drawx, drawo]
-
+board=[-1,-1,-1,-1,-1,-1,-1,-1,-1]
 
 def tap(x, y):
     """Draw X or O in tapped square."""
     x = floor(x)
     y = floor(y)
+    print(x,y)
+    boxpos=box(x,y)
+    print(boxpos)
     player = state['player']
     draw = players[player]
     draw(x, y)
     update()
     state['player'] = not player
-
+  
+def box(x, y):
+    if x==-52 and y==-52:
+       return 4
 
 setup(420, 420, 370, 0)
 hideturtle()
